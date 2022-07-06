9664209f90f4f0dce864393e39f9e32792804182
limjonghyeok <42922673+limjongheok@users.noreply.github.com>
2022-07-07 00:21:58 +0900

    [Silver I] Title: 음식물 피하기, Time: 372 ms, Memory: 29480 KB -BaekjoonHub

---
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
@@ -0,0 +1,92 @@
+import java.util.*;
+
+
+public class Main {
+    static int N;
+    static  int M;
+    static int K;
+    static int[][] point;
+    static int[] dx = {-1,0,1,0};
+    static int[] dy = {0,1,0,-1};
+    static boolean[][] check;
+    static int len;
+    static int max;
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        K = sc.nextInt();
+
+
+        point = new  int[N][M];
+        check = new boolean[N][M];
+
+        // 좌표 받기
+        for(int i =0; i<K; i++){
+            int one = (sc.nextInt());
+            int two = (sc.nextInt());
+            point[one-1][two-1] = 1;
+
+        }
+        // 좌표에 값 넣기
+
+
+
+        //확인
+//        for(int i=0; i<N; i++){
+//            for(int j=0; j<M; j++){
+//                System.out.print(point[i][j]);
+//            }
+//            System.out.println(" ");
+//        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if(!check[i][j] && point[i][j]==1) {
+                    len=0;
+                    bfss(i,j);
+                    max = Math.max(len, max);
+                }
+            }
+        }
+        System.out.println(max);
+
+
+    }
+
+    public static void bfss(int x, int y) {
+        check[x][y] = true;
+        len++;
+        Queue<dots> queue = new LinkedList<>();
+        queue.add(new dots(x,y));
+
+        while(!queue.isEmpty()){
+
+            dots d = queue.poll(); //다음 큐
+
+            for(int i=0; i<4; i++){
+                int xx = d.x + dx[i];
+                int yy = d.y + dy[i];
+
+                if(xx<0 || yy<0 || xx>=N || yy>=M)continue;
+                if(!check[xx][yy] && point[xx][yy]==1) {
+                    queue.add(new dots(xx, yy));
+                    check[xx][yy]=true;
+                    len++;
+                }
+            }
+
+        }
+
+
+    }
+}
+class dots{
+    int x;
+    int y;
+    public dots(int x, int y){
+        this.x = x;
+        this.y =y;
+    }
+}
