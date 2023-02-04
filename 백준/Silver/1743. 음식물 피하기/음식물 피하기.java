644a05f93d1bf678ad431a4f7304ac44383df75b
limjonghyeok <42922673+limjongheok@users.noreply.github.com>
2023-02-04 14:18:37 +0900

    [Silver I] Title: 음식물 피하기, Time: 180 ms, Memory: 15532 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
@@ -1,88 +1,117 @@
-import java.util.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static  int M;
-    static int K;
-    static int[][] point;
-    static int[] dx = {-1,0,1,0};
-    static int[] dy = {0,1,0,-1};
+
+    static  int N; // 통로 세로 길이
+    static  int M;// 통로 가로 길이
+    static  int K; // 음식물 떨어진 좌표 개수
+
+    static  int[] dx = {0,1,0,-1};
+    static  int[] dy = {1,0,-1,0};
+
+
     static boolean[][] check;
-    static int len;
-    static int max;
+    static boolean[][] board;
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        K = sc.nextInt();
-        
+    static  int count = 0;
 
-        point = new  int[N][M];
+    static  int ans;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st;
+        st = new StringTokenizer(bf.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        board = new boolean[N][M];
+
+
         check = new boolean[N][M];
 
-        // 좌표 받기
-        for(int i =0; i<K; i++){
-            int one = (sc.nextInt());
-            int two = (sc.nextInt());
-            point[one-1][two-1] = 1;
+        // 음식물 채우기
+        for(int i=0; i<K; i++){
+            st = new StringTokenizer(bf.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
 
+            board[x-1][y-1]  = true;
         }
-        // 좌표에 값 넣기
-
-
 
         for(int i=0; i<N; i++){
             for(int j=0; j<M; j++){
-                if(check[i][j]== false && point[i][j] ==1){
-                    len =0;
-                    bfss(i,j); // 연결트리 true 설정
-                    max = Math.max(len,max);
+                if(board[i][j] == true && !check[i][j]){
 
 
-
-
+                    dfs(i,j); // dfs 마다 ans가 알맞게 세짐
+                    count = Math.max(count, ans);
                 }
+
             }
         }
-        System.out.println(max);
 
+        System.out.println(count);
 
     }
+    public static  void dfs(int x, int y ){
 
-    public static void bfss(int x, int y) {
+
+
+        Stack<dot> stack = new Stack<>();
+        stack.add(new dot(x,y));
         check[x][y] = true;
-        len++;
-        Queue<dots> queue = new LinkedList<>();
-        queue.add(new dots(x,y));
 
-        while(!queue.isEmpty()){
+        ans = 0;
 
-            dots d = queue.poll(); //다음 큐
+
+        while(!stack.isEmpty()){
+            dot dot = stack.pop();
+
+            ans++;
+
+            int nowX = dot.x;
+            int nowY = dot.y;
 
             for(int i=0; i<4; i++){
-                int xx = d.x + dx[i];
-                int yy = d.y + dy[i];
+                int nextX = nowX+ dx[i];
+                int nextY = nowY + dy[i];
 
-                if(xx<0 || yy<0 || xx>=N || yy>=M)continue;
-                if(!check[xx][yy] && point[xx][yy]==1) {
-                    queue.add(new dots(xx, yy));
-                    check[xx][yy]=true;
-                    len++;
+                if(nextX >=0 && nextX<N){
+                    if(nextY>=0 && nextY<M){
+                        if(check[nextX][nextY] == false){
+                            if(board[nextX][nextY] == true){
+
+                                check[nextX][nextY] = true; // 방문 시켜주고
+                                stack.add(new dot(nextX,nextY)); // 스택에 담기
+                            }
+
+
+                        }
+                    }
+
+
                 }
             }
-
         }
 
 
+
     }
-}
-class dots{
-    int x;
-    int y;
-    public dots(int x, int y){
-        this.x = x;
-        this.y =y;
+    private  static  class  dot{
+        int x;
+        int y;
+        public dot(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
     }
 }
