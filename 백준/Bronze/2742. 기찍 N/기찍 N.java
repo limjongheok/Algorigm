54eeadd75f196eb902fd5df9e16222366ddfb339
limjonghyeok <42922673+limjongheok@users.noreply.github.com>
2023-10-15 00:02:45 +0900

    [Bronze IV] Title: 기찍 N, Time: 1024 ms, Memory: 30100 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for(int i=n; i>0; i--){
+            System.out.println(i);
+        }
+        
+        
+    }
+}
