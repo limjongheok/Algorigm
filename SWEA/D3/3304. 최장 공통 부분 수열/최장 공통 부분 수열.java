46cc7bbb83f077182fbc5acf36d133a79ffc5419
limjonghyeok <42922673+limjongheok@users.noreply.github.com>
2023-11-18 20:59:35 +0900

    [D3] Title: 최장 공통 부분 수열, Time: 169 ms, Memory: 32,076 KB -BaekjoonHub

--- SWEA/D3/3304. 최장 공통 부분 수열/최장 공통 부분 수열.java
+++ SWEA/D3/3304. 최장 공통 부분 수열/최장 공통 부분 수열.java
@@ -34,7 +34,6 @@ import java.util.*;
  */
 class Solution
 {
-    private static int[][] dp;
 	public static void main(String args[]) throws Exception
 	{
 		/*
@@ -57,31 +56,28 @@ class Solution
 		*/
         StringTokenizer st;
 
-		for(int tc = 1; tc <= T; tc++)
+		for(int test_case = 1; test_case <= T; test_case++)
 		{
             st = new StringTokenizer(br.readLine());
-       		char[] 	X = st.nextToken().toCharArray();
-            char[] Y = st.nextToken().toCharArray();
+            String s = st.nextToken();
+            String s2 = st.nextToken();
             
-            int m = X.length;
-            int n = Y.length;
+            int n1 = s.length();
+            int n2 = s2.length();
             
-            dp = new int[m+1][n+1];
+            int[][] dp = new int[n1+1][n2+1];
             
-            for(int i=0; i<=m; i++){
-            	for(int j=0; j<=n; j++){
-                   if( i==0 || j==0 ) {
-						dp[i][j] = 0;
-						continue;
-					}
-					if( X[i-1] == Y[j-1] ) {
-						dp[i][j] = dp[i-1][j-1]+1;
-						continue;
-					}
-					dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
-				}
+            for(int i=1; i<=n1; i++){
+            	for(int j=1; j<=n2; j++){
+                    if(s.charAt(i-1) == s2.charAt(j-1)){
+                    	dp[i][j] = dp[i-1][j-1] +1;
+                     }else{
+                    	dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                    }
+                
+                }
             }
-           System.out.println("#" + tc +" " +dp[m][n]);
+            System.out.println("#" + test_case +" " +dp[n1][n2]);
                        
 		
 			/////////////////////////////////////////////////////////////////////////////////////////////
