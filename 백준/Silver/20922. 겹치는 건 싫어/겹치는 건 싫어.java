ac0a0103cfb29c1d2d526ae5dc65070afe5c8dc4
limjonghyeok <42922673+limjongheok@users.noreply.github.com>
2024-01-21 17:27:28 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 348 ms, Memory: 32984 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -2,44 +2,55 @@ import java.io.*;
 import java.util.*;
 
 class Main{
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        static int []arr;
-        static int []cnt;
-        public static void main(String[] args) throws IOException {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());
-            arr = new int[n];
-            cnt = new int[100001];
-            st = new StringTokenizer(br.readLine());
-            for(int i=0;i<n;i++)
-                arr[i] = Integer.parseInt(st.nextToken());
+        StringTokenizer st;
 
-            int l =0, r= 0;
-            int ans = 0 ;
-            while(l<=r)
-            {
-              if(r<=n-1 &&cnt[arr[r]] <k)
-              {
-                  cnt[arr[r]]++;
-                  r++;
+        st = new StringTokenizer(br.readLine());
 
-              }
-              else if(cnt[arr[r]] == k)
-              {
-                  cnt[arr[l]]--;
-                  l++;
-              }
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-                ans = Math.max(ans, r - l);
-                if(r == n)
-                    break;
-            }
-            System.out.println(ans);
+        int[] arr = new int[n];
+        int[] check = new int[100001];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<arr.length; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-    
+        int start = 0;
+        int end = 0;
+        int max = 0;
 
+        while(true){
+            if(end == n){
+                break;
+            }
+        
+
+            int len = end -start;
+            int now = arr[end];
+
+            if(check[now] < k){
+                check[now] += 1;
+
+                if(max < len){
+                    max = len;
+                }
+                end = end +1;
+
+            }else if(check[now] >= k){
+                int sPoint = arr[start];
+                check[sPoint] -= 1;
+                start = start +1;
+            }
+        }
+        if(n== 0){
+            System.out.println(0);
+        }else{
+            System.out.println(max+1);
+        }
+    }
 }
