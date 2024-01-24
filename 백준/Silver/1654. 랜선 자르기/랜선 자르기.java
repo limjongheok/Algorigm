b9e0dc1d6021defb2fad55f9682d11ec1b80da9f
limjonghyeok <42922673+limjongheok@users.noreply.github.com>
2024-01-25 00:53:28 +0900

    [Silver II] Title: 랜선 자르기, Time: 180 ms, Memory: 17420 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -2,68 +2,42 @@ import java.io.*;
 import java.util.*;
 
 class Main{
-    public static void main(String[] args) throws IOException {
-
-
+   private static StringBuilder sb = new StringBuilder();
+    public static  void main(String[] args) throws  IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st;
+
         st = new StringTokenizer(br.readLine());
-        int k = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
+        long[] arr = new long[n];
 
-        int[] num = new int[k];
-
-        long max = 0;
-
-        // 입력과 동시에 해당 랜선의 길이가 최댓값인지를 확인하고 max를 갱신
-        for(int i=0; i<k; i++){
-            num[i] = Integer.parseInt(br.readLine());
-            if(max<num[i]){
-                max = num[i];
+        long start =1;
+        long end = 0;
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+            if(end < arr[i]){
+                end = arr[i];
             }
         }
+        end ++;
 
+        while(start < end){
+            long mid = (start + end) /2;
 
-        // 반드시 max에서 +1 값이어야 한다.
-        max++;
-
-        long min = 0; // 탐색 길이 최솟값
-        long mid = 0;
-
-        while (min < max) {
-
-            // 범위 내에서 중간 길이를 구한다.
-            mid = (max + min) / 2;
-
-
-            long count = 0;
-
-            // 구해진 중간 길이로 잘라서 총 몇 개가 만들어지는지를 구한다.
-
-            for (int i = 0; i < num.length; i++) {
-                count += (num[i] / mid);
+            long sum = 0;
+            for(int i=0; i<n; i++){
+                sum += (arr[i] / mid);
             }
-
-            /*
-             *  [upper bound 형식]
-             *
-             *  mid길이로 잘랐을 때의 개수가 만들고자 하는 랜선의 개수보다 작다면
-             *  자르고자 하는 길이를 줄이기 위해 최대 길이를 줄인다.
-             *  그 외에는 자르고자 하는 길이를 늘려야 하므로 최소 길이를 늘린다.
-             */
-            if(count < n) {
-                max = mid;
+            // 최대 랜선
+            if(sum < m){
+                end = mid;
+            }else{
+                start = mid +1;
             }
-            else {
-                min = mid+1 ;
-            }
-
-
         }
+        System.out.println(end-1);
 
-        // UpperBound로 얻어진 값(min)에 -1이 최대 길이가 된다.
-        System.out.println(min - 1);
     }
 }
