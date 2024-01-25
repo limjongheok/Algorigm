17f789f5c9ef425269901d7be614a7e4b9f97c26
limjonghyeok <42922673+limjongheok@users.noreply.github.com>
2024-01-25 23:49:38 +0900

    [Bronze IV] Title: 세수정렬, Time: 140 ms, Memory: 15828 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[3];
+        for(int i=0; i<3; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+
+        for(int i : arr){
+            System.out.print(i + " ");
+        }
+
+    }
+}
