a9be46c4c9e309ead5793a6d7ba4e4f39a86f099
limjonghyeok <42922673+limjongheok@users.noreply.github.com>
2024-01-30 13:56:30 +0900

    [Silver III] Title: N과 M (1), Time: 1072 ms, Memory: 59704 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -2,49 +2,41 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    private static int[] arr;
     private static int n;
-    private static int m;
-    private static boolean[]visited;
+    private static  int m;
+    private static boolean[] visited;
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+         n = Integer.parseInt(st.nextToken());
+         m = Integer.parseInt(st.nextToken());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+         visited = new boolean[n];
+         int[] arr = new int[m];
 
-        arr = new int[m];
-        visited = new boolean[n];
-
-
-
-        dfs(0);
-
+        re(0, arr);
     }
 
-    private static void dfs(int start){
-
-
-        if(start==m){
-            for(int i : arr){
-                System.out.print(i + " ");
+    private static void re(int idx, int[] arr){
+        if(idx == m){
+            StringBuilder sb = new StringBuilder();
+            for(int i: arr){
+                sb.append(i+ " ");
             }
-            System.out.println();
+            System.out.println(sb);
             return;
         }
 
         for(int i=0; i<n; i++){
             if(!visited[i]){
                 visited[i] = true;
-                arr[start] = i +1 ;
-                dfs(start+1);
+                arr[idx] = i+1;
+                re(idx+1,arr);
                 visited[i] = false;
             }
         }
-
     }
-
 
 }
