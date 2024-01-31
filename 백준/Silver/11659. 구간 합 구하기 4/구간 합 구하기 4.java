3bd493f5dd90eb29ba047c7aab7f473ba1d8e4ea
limjonghyeok <42922673+limjongheok@users.noreply.github.com>
2024-01-31 16:48:08 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1596 ms, Memory: 59200 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,38 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 public class Main {
-    static int sum;
-    static  int array[];
-
-    static  int N;
-    static  int M;
-
-
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[n+1];
 
-        StringTokenizer st ;
-        st = new StringTokenizer(bf.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        array = new int[N+1];
-
-        st = new StringTokenizer(bf.readLine()); // 5 4 3 2 1
-        for(int i=1; i<=N; i++){
-            array[i] = array[i-1]+ Integer.parseInt(st.nextToken());
-        }
-        for(int i=0;i<M;i++) {
-            st=new StringTokenizer(bf.readLine());
-            int a=Integer.parseInt(st.nextToken());
-            int b=Integer.parseInt(st.nextToken());
-            // a, b사이의 구간합은 array[b]-array[a-1]과 같다
-            System.out.println(array[b]-array[a-1]);
+        st = new StringTokenizer(br.readLine());
+        for(int i =1; i<=n; i++){
+            arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
         }
 
+        for(int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int one = (Integer.parseInt(st.nextToken())-1);
+            int two = (Integer.parseInt(st.nextToken()));
+
+            int result = arr[two] - arr[one];
+            System.out.println(result);
+
+
+        }
     }
+
 }
