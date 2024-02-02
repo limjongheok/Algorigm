5fa76b906382f398275c0abed8237c375d0a2cce
limjonghyeok <42922673+limjongheok@users.noreply.github.com>
2024-02-02 14:40:17 +0900

    [Silver IV] Title: 카드2, Time: 200 ms, Memory: 53384 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,22 +1,31 @@
-import java.io.*;
-import java.util.*;
-class Main{
-    public static void main(String[] arsg) throws IOException{
-        Deque<Integer> dq = new ArrayDeque();
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         int n = Integer.parseInt(br.readLine());
-        
-        for(int i=1; i<=n; i++){
-            dq.add(i);
+
+        Deque<Integer> queue = new LinkedList<>();
+        for(int i=0; i<n; i++){
+            queue.add(i+1);
         }
-        while(dq.size() !=1){
-            dq.remove();
-            int n2 = dq.remove();
-            dq.add(n2);
+
+        while(queue.size()!=1){
+            queue.pollFirst();// 가장 첫번째꺼 삭제
+
+            int delete = queue.pollFirst(); //
+            queue.add(delete); // 가장 뒤에 추가
         }
-        System.out.println(dq.getFirst());
-        
-        
+
+        System.out.println(queue.poll());
+
+
+
     }
 }
