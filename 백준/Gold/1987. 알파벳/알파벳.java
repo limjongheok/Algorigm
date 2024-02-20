45e76b830dd859cf539a893fdc454c2636fc3d40
limjonghyeok <42922673+limjongheok@users.noreply.github.com>
2024-02-20 14:47:43 +0900

    [Gold IV] Title: 알파벳, Time: 864 ms, Memory: 15164 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,52 +1,67 @@
+import java.io.*;
+import java.util.StringTokenizer;
 
-import java.util.Scanner;
 
 public class Main {
-    static int R, C;
-    static int[][] bord;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int  count = 0;
-    static boolean[] check;
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
+	private static boolean[] visited;
+	private static int[] dx =  {1,-1,0,0};
+	private static int[] dy = {0,0,1,-1};
+	private static char[][] arr;
+	private static int n;
+	private static int m;
+	private static int max;
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		visited= new boolean[27]; // 알파벳 방문 배열 
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		 n = Integer.parseInt(st.nextToken());
+		 m = Integer.parseInt(st.nextToken());
+		
+		arr = new char[n][m];
+		for(int i=0; i<n; i++) {
+			String s = br.readLine();
+			for(int j =0; j<m; j++) {
+				arr[i][j] = s.charAt(j);
+				visited[arr[i][j]-65] = true;
+			}
+		}
+		max = Integer.MIN_VALUE;
+		visited[arr[0][0]-65] = false;
+		dfs(0,0, 0);
+		System.out.println(max+1);
+		
+		
+	}
+	
+	private static void dfs(int x, int y , int count  ) {
+		
+		
+		
+		for(int i=0; i<4; i++) {
+			int nextX = x + dx[i];
+			int nextY = y + dy[i];
+			
+			if(nextX >=0 && nextX <n ) {
+				if(nextY >=0 && nextY <m) {
+					if(visited[arr[nextX][nextY]-65]) {
+						visited[arr[nextX][nextY]-65] = false;
+						dfs(nextX,nextY,count+1);
+						visited[arr[nextX][nextY]-65] = true;
+					}
+				}
+			}
+		}
+		
+		max = Math.max(max, count);
+		
+		
+		
+		
+		
+	}
+	
 
-        R = scan.nextInt();
-        C = scan.nextInt();
-        scan.nextLine();
-
-        bord = new int[R][C];
-        for(int i = 0; i < R; i++) {
-            String str = scan.nextLine();
-            for(int j = 0; j < C; j++) {
-                bord[i][j] = str.charAt(j) - 'A';
-            }
-        }
-
-        check = new boolean[26];
-
-
-        dfs(0,0,1);
-         System.out.println(count);
-
-
-
-    }
-
-    private static void dfs(int i, int j,int len) {
-        check[bord[i][j]] = true;
-         count= Math.max(count, len);
-
-        for(int k = 0; k < 4; k++) {
-            int nx = i + dx[k];
-            int ny = j + dy[k];
-            if(nx >= 0 && ny >= 0 && nx < R && ny < C) {
-                if(check[bord[nx][ny]] == false) {
-                    dfs(nx, ny, len + 1);
-                    check[bord[nx][ny]] = false;
-                }
-            }
-        }
-
-    }
 }
