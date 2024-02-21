dc8ef6cb4a22aaf79e95f209af76b63da8d567ea
limjonghyeok <42922673+limjongheok@users.noreply.github.com>
2024-02-21 11:03:56 +0900

    [Gold V] Title: 집합의 표현, Time: 468 ms, Memory: 53788 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,77 +1,69 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] parent;
- 
-    public static void main(String[] args) throws NumberFormatException, IOException {
+ private static int[] arr;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
- 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
- 
-        parent = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            parent[i] = i;
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        arr = new int[n+1];
+
+        for(int i=0; i<=n; i++){ // n+1 까지 처음 합 집합 공간은 0으로 있음
+            arr[i] = i;
         }
- 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < M; i++) {
+
+        for(int i=0; i<m; i++) { // 연산 수
+
             st = new StringTokenizer(br.readLine());
-            int command = Integer.parseInt(st.nextToken());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
- 
-            if (command == 0) {
-                union(a, b);
-            } else if (command == 1) {
-                sb.append((isSameParent(a, b) ? "YES" : "NO") + "\n");
-            } else {
-                continue;
+            int cal = Integer.parseInt(st.nextToken()); // 연산
+            int one = Integer.parseInt(st.nextToken()); // 첫 집합수
+            int two = Integer.parseInt(st.nextToken()); //두번째 집합수
+
+            switch (cal){
+                case 0: // 합침
+                    union(one, two);
+                    break;
+                case 1:
+                    int first = find(one);
+                    int sec = find(two);
+                    if(first == sec){
+                        sb.append("YES");
+                        sb.append("\n");
+                    }else {
+                        sb.append("NO");
+                        sb.append("\n");
+                    }
             }
         }
- 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(sb);
     }
- 
-    // x의 부모를 찾는 연산
-    public static int find(int x) {
-        if (x == parent[x]) {
-            return x;
-        }
- 
-        return parent[x] = find(parent[x]);
-    }
- 
-    // y의 부모를 x의 부모로 치환하는 연산 (x > y 일 경우, 반대)
-    public static void union(int x, int y) {
-        x = find(x);
-        y = find(y);
- 
-        if (x != y) {
-            if (x < y) {
-                parent[y] = x;
-            } else {
-                parent[x] = y;
+
+    private static void union(int one, int two){
+       int first = find(one);
+       int second = find(two); // 제일 루트 연결 좌표를 찾아서 더 작은 거랑 연결
+
+        if(first!=second){
+            if(first > second){
+                arr[first] = second;
+            }else{
+                arr[second] = first;
             }
         }
     }
- 
-    // x와 y의 부모가 같은지 확인
-    public static boolean isSameParent(int x, int y) {
-        x = find(x);
-        y = find(y);
- 
-        if (x == y) {
-            return true;
+
+    private static int find(int num){
+        if(arr[num] == num){ // 만약 내가 배열의 나의 위치랑 같으면 루트
+            return num;
+        }else{
+            return arr[num] = find(arr[num]); // 아니면 해당 노드에 들어감
         }
- 
-        return false;
     }
- 
 }
+
